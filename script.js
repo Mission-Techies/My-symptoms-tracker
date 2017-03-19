3f2c6d32181c3b84328d64cdbda8ab30f2067083
brendamor <brendamorales@missions-Air-2.attlocal.net>
2017-03-19 10:41:41 -0700

    progress

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var techies_data_url = 'https://airtable.com/tbl44bjro8pJGh9vi/viwDgx01MeBnvgeE0';
-var techiesHTML = '';
-var techiesDiv = $('.techies');
-var renderTechiesData = function (data) {
-  data.records.forEach(function(techie) {
-    techiesHTML += '<h2>' + techie.fields['Name'] + '</h2>';
-    techiesHTML += '<a href="' + techie.fields['Website'] + '">' + techie.fields['Website'] + '</a>';
-    techiesHTML += '<p>' + techie.fields['Bio'] + '</p>';
-    techiesHTML += '<hr />';
+var brendamor_data_url = 'https://api.airtable.com/v0/appEmW1lSuM0fBVev/Symptoms?api_key=keybIfILI5Qa2691W';
+var brendamorHTML = '';
+var brendamorDiv = $('.symptoms');
+var renderbrendamorData = function (data) {
+  data.records.forEach(function(brendamor) {
+    brendamorHTML += '<h2>' + brendamor.fields['Name'] + '</h2>';
+    brendamorHTML += '<p>' + brendamor.fields['Description'] + '</p>';
+    brendamorHTML += '<hr />';
   })
-  techiesDiv.html(techiesHTML);
+  brendamorDiv.html(brendamorHTML);
+  $('.general').append(brendamorHTML);
 }
 
 //
-$.getJSON(techies_data_url, renderTechiesData);
+$.getJSON(brendamor_data_url, renderbrendamorData);
